742ab5a8ee4b5dcc765ed9ddb9cfd65f9a9462ee
huan.yang <huan.yang@longsys.com>
2026-05-08 11:30:25 +0800

    chore: disk module.

---
+++ src/include/disk/disk_manager.h
@@ -0,0 +1,16 @@
+//
+// Created by huan.yang on 2026-05-08.
+//
+#pragma once
+
+namespace chickenDB {
+
+    class DiskManager {
+    public:
+        DiskManager() =  default;
+        virtual ~DiskManager() = default;
+
+
+    };
+
+}
